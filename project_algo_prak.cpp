28f56abf5d0b9e2f22a42778c70a42beac894808
AzzamFarrosyaffa <azzamfarrosyaffa@gmail.com>
2026-05-29 14:15:13 +0700

    finish

--- project_algo_prak.cpp
+++ project_algo_prak.cpp
@@ -2,22 +2,42 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <cctype>
 
 using namespace std;
 
-// struct untuk menyimpan kontak
+// STRUCT
 struct Kontak {
     string nama;
     string noTelepon;
-    int kategori; // 1: Keluarga, 2: Saudara, 3: Teman, 4: Tetangga
+    int kategori;
 };
 
-// Global Variables
+// GLOBAL VARIABLE
 Kontak daftarKontak[1000];
 int jumlahKontak = 0;
+const int MAX_KONTAK = 1000;
 const string database = "contact_database.txt";
 
-// --- FUNGSI HELPER ---
+// PROTOTYPE FUNCTION
+string getNamaKategori(int k);
+void tukarKontak(Kontak* a, Kontak* b);
+bool nomorSudahAda(string noHP);
+bool nomorValid(string noHP);
+void cinFail();
+void appendDatabase();
+void overwriteDatabase();
+void loadDatabase();
+int hitungPerKategori(int index, int target);
+int partition(int low, int high, bool asc);
+void quickSort(int low, int high, bool asc);
+int binarySearch(string target);
+void tambahKontak();
+void tampilkanTabel(int filterKategori = 0);
+void cariEditHapus();
+void menuStatistik();
+
+// HELPER FUNCTION
 string getNamaKategori(int k) {
     if (k == 1) return "Keluarga";
     if (k == 2) return "Saudara";
@@ -26,199 +46,384 @@ string getNamaKategori(int k) {
     return "Lainnya";
 }
 
-// PENERAPAN POINTER 1: Fungsi untuk menukar (swap) data menggunakan parameter pointer
 void tukarKontak(Kontak* a, Kontak* b) {
     Kontak temp = *a;
     *a = *b;
     *b = temp;
 }
 
-// --- FUNGSI FILE HANDLING ---
-void storeDatabase() {
-    ofstream file(database);
+// mengecek apakah nomor ganda?
+bool nomorSudahAda(string noHP) {
     for (int i = 0; i < jumlahKontak; i++) {
-        file << daftarKontak[i].nama << "|" 
-             << daftarKontak[i].noTelepon << "|" 
+        if (daftarKontak[i].noTelepon == noHP) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// mengecek apakah nomor hanya terdiri dari angka
+bool nomorValid(string noHP) {
+    for (size_t i = 0; i < noHP.length(); i++) {
+        if (!isdigit(noHP[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void cinFail() {
+    cin.clear();
+    cin.ignore(1000, '\n');
+    cout << "Input tidak valid!\n";
+}
+
+// menambahkan 1 data kontak baru ke file tanpa menghapus isi lama file
+void appendDatabase() {
+    ofstream file(database, ios::app);
+    if (!file.is_open()) {
+        cout << "Gagal membuka file!\n";
+        return;
+    }
+
+    Kontak& k = daftarKontak[jumlahKontak - 1];
+    file << k.nama << "|"
+         << k.noTelepon << "|"
+         << k.kategori << endl;
+
+    file.close();
+}
+
+// menulis ulang seluruh data kontak ke file (saat edit dan hapus)
+void overwriteDatabase() {
+    ofstream file(database, ios::trunc);
+    if (!file.is_open()) {
+        cout << "Gagal membuka file!\n";
+        return;
+    }
+    for (int i = 0; i < jumlahKontak; i++) {
+        file << daftarKontak[i].nama << "|"
+             << daftarKontak[i].noTelepon << "|"
              << daftarKontak[i].kategori << endl;
     }
     file.close();
 }
 
+//membaca data dari file lalu memasukkannya ke dalam array daftarKontak.
 void loadDatabase() {
     ifstream file(database);
-    if (!file) return;
-
+    if (!file.is_open()) {
+        return;
+    }
     jumlahKontak = 0;
-    string line;
-    while (getline(file, line)) {
-        size_t p1 = line.find("|");
-        size_t p2 = line.rfind("|");
-        if (p1 != string::npos && p2 != string::npos) {
-            daftarKontak[jumlahKontak].nama = line.substr(0, p1);
-            daftarKontak[jumlahKontak].noTelepon = line.substr(p1 + 1, p2 - p1 - 1);
-            daftarKontak[jumlahKontak].kategori = stoi(line.substr(p2 + 1));
-            jumlahKontak++;
-        }
+    string nama;
+    string noHP;
+    string kategoriStr;
+    while (
+        getline(file, nama, '|') &&
+        getline(file, noHP, '|') &&
+        getline(file, kategoriStr)
+    ) {
+        daftarKontak[jumlahKontak].nama = nama;
+        daftarKontak[jumlahKontak].noTelepon = noHP;
+        daftarKontak[jumlahKontak].kategori = stoi(kategoriStr);
+
+        jumlahKontak++;
     }
     file.close();
 }
 
-// fungsi rekursif untuk menampilkan statistik
+// REKURSI menghitung kategori
 int hitungPerKategori(int index, int target) {
-    if (index >= jumlahKontak) return 0;
+    if (index >= jumlahKontak) {
+        return 0;
+    }
     int point = (daftarKontak[index].kategori == target) ? 1 : 0;
     return point + hitungPerKategori(index + 1, target);
 }
 
-// fungsi sorting menggunakan bubble sort
-void urutkanData(bool asc) {
-    for (int i = 0; i < jumlahKontak - 1; i++) {
-        for (int j = 0; j < jumlahKontak - i - 1; j++) {
-            bool syarat = asc ? (daftarKontak[j].nama > daftarKontak[j+1].nama) 
-                              : (daftarKontak[j].nama < daftarKontak[j+1].nama);
-            if (syarat) {
-                // Memanggil fungsi swap dengan mengirim alamat memori (pointer)
-                tukarKontak(&daftarKontak[j], &daftarKontak[j+1]);
-            }
+// QUICK SORT
+int partition(int low, int high, bool asc) {
+    string pivot = daftarKontak[high].nama;
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        bool kondisi;
+        if (asc) {
+            kondisi = daftarKontak[j].nama < pivot;
+        } else {
+            kondisi = daftarKontak[j].nama > pivot;
         }
+        if (kondisi) {
+            i++;
+            tukarKontak(&daftarKontak[i], &daftarKontak[j]);
+        }
+    }
+    tukarKontak(&daftarKontak[i + 1], &daftarKontak[high]);
+    return i + 1;
+}
+
+void quickSort(int low, int high, bool asc) {
+    if (low < high) {
+        int pi = partition(low, high, asc);
+        quickSort(low, pi - 1, asc);
+        quickSort(pi + 1, high, asc);
     }
 }
 
+// BINARY SEARCH
+int binarySearch(string target) {
+    int left = 0;
+    int right = jumlahKontak - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (daftarKontak[mid].nama == target) {
+            return mid;
+        }
+        else if (daftarKontak[mid].nama < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+// TAMBAH KONTAK
 void tambahKontak() {
-    cout << "\n--- TAMBAH KONTAK BARU ---\n";
-    cout << "Nama           : "; getline(cin >> ws, daftarKontak[jumlahKontak].nama);
-    cout << "Nomor Telepon  : "; cin >> daftarKontak[jumlahKontak].noTelepon;
-    cout << "Kategori (1:Keluarga, 2:Saudara, 3:Teman, 4:Tetangga): ";
-    cin >> daftarKontak[jumlahKontak].kategori;
-    
+    if (jumlahKontak >= MAX_KONTAK) {
+        cout << "Database penuh!\n";
+        return;
+    }
+    cout << "\n--- TAMBAH KONTAK ---\n";
+    cout << "Nama : ";
+    getline(cin >> ws, daftarKontak[jumlahKontak].nama);
+    do {
+        cout << "Nomor HP : ";
+        cin >> daftarKontak[jumlahKontak].noTelepon;
+        if (!nomorValid(daftarKontak[jumlahKontak].noTelepon)) {
+            cout << "Nomor HP hanya boleh angka!\n";
+            continue;
+        }
+        if (nomorSudahAda(daftarKontak[jumlahKontak].noTelepon)) {
+            cout << "Nomor HP sudah terdaftar!\n";
+            continue;
+        }
+        break;
+    } while (true);
+
+    do {
+        cout << "Kategori (1-4): ";
+        cin >> daftarKontak[jumlahKontak].kategori;
+    } while (
+        daftarKontak[jumlahKontak].kategori < 1 ||
+        daftarKontak[jumlahKontak].kategori > 4
+    );
     jumlahKontak++;
-    storeDatabase();
-    cout << ">> Kontak berhasil disimpan!\n";
+    appendDatabase();
+    cout << ">> Kontak berhasil ditambahkan!\n";
 }
 
-void tampilkanTabel(int filterKategori = 0) {
-    cout << "\n" << setfill('=') << setw(66) << "=" << endl;
-    cout << setfill(' ') << setw(5) << "No" << setw(11) << "Nama" 
-         << setw(27) << "Nomor HP" << setw(20) << "Kategori" << endl;
-    cout << setfill('=') << setw(66) << "=" << setfill(' ') << endl;
+// TAMPILKAN TABEL
+void tampilkanTabel(int filterKategori) {
+    cout << "\n"
+         << setfill('=')
+         << setw(70)
+         << "=" << endl;
 
-    int counter = 1;
+    cout << left
+         << setfill(' ')
+         << setw(5) << "No"
+         << setw(25) << "Nama"
+         << setw(20) << "Nomor HP"
+         << setw(15) << "Kategori"
+         << endl;
+
+    cout << setfill('=')
+         << setw(70)
+         << "="
+         << setfill(' ')
+         << endl;
+
+    int no = 1;
     for (int i = 0; i < jumlahKontak; i++) {
-        if (filterKategori == 0 || daftarKontak[i].kategori == filterKategori) {
-            cout << "   " << left << setw(5) << counter++ 
-                 << left << setw(25) << daftarKontak[i].nama 
-                 << left << setw(15) << daftarKontak[i].noTelepon 
-                 << right << setw(15) << getNamaKategori(daftarKontak[i].kategori) << endl;
+        if (
+            filterKategori == 0 ||
+            daftarKontak[i].kategori == filterKategori
+        ) {
+            cout << left
+                 << setw(5) << no++
+                 << setw(25) << daftarKontak[i].nama
+                 << setw(20) << daftarKontak[i].noTelepon
+                 << setw(15)
+                 << getNamaKategori(daftarKontak[i].kategori)
+                 << endl;
         }
     }
-    cout << setfill('-') << setw(66) << "-" << endl;
+    cout << setfill('-')
+         << setw(70)
+         << "-"
+         << endl;
 }
 
+// CARI / EDIT / HAPUS
 void cariEditHapus() {
     string cari;
-    string ulang;
-    do {
-        system("cls");
-		cout << "\nCari Nama atau No HP (Case Sensitive) : "; getline(cin >> ws, cari);
-		int indexKetemu = -1;
+    quickSort(0, jumlahKontak - 1, true);
+    cout << "\nCari Nama : ";
+    getline(cin >> ws, cari);
+    int indexKetemu = binarySearch(cari);
+    if (indexKetemu == -1) {
+        cout << "Data tidak ditemukan!\n";
+        return;
+    }
 
-		for (int i = 0; i < jumlahKontak; i++) {
-			if (daftarKontak[i].nama == cari || daftarKontak[i].noTelepon == cari) {
-				indexKetemu = i;
-				break;
-			}
-		}
+    Kontak* p = &daftarKontak[indexKetemu];
 
-		if (indexKetemu != -1) {
-			// pointer struct yang menunjuk ke kontak yang ditemukan
-			Kontak* pointerEdit = &daftarKontak[indexKetemu];
+    cout << "\n=== DATA DITEMUKAN ===\n";
+    cout << "Nama     : " << p->nama << endl;
+    cout << "No HP    : " << p->noTelepon << endl;
+    cout << "Kategori : "
+         << getNamaKategori(p->kategori)
+         << endl;
 
-			cout << "\n[ DATA DITEMUKAN ]\n";
-			// operator arrow (->) untuk mengakses member struct via pointer
-			cout << "Nama     : " << pointerEdit->nama << endl;
-			cout << "No HP    : " << pointerEdit->noTelepon << endl;
-			cout << "Kategori : " << getNamaKategori(pointerEdit->kategori) << endl;
+    int action;
+    cout << "\n1. Edit\n";
+    cout << "2. Hapus\n";
+    cout << "3. Kembali\n";
+    cout << "Pilih : ";
+    cin >> action;
+    if (action == 1) {
+        cout << "Nama Baru : ";
+        getline(cin >> ws, p->nama);
+        do {
+            cout << "No HP Baru : ";
+            cin >> p->noTelepon;
+            if (!nomorValid(p->noTelepon)) {
+                cout << "Nomor hanya boleh angka!\n";
+                continue;
+            }
+            break;
+        } while (true);
+        do {
+            cout << "Kategori Baru (1-4): ";
+            cin >> p->kategori;
 
-			cout << "\n1. Edit | 2. Hapus | 3. Kembali: ";
-			int action; cin >> action;
+        } while (
+            p->kategori < 1 ||
+            p->kategori > 4
+        );
+        overwriteDatabase();
+        cout << ">> Data berhasil diupdate!\n";
+    }
+    else if (action == 2) {
+        char yakin;
+        cout << "Yakin ingin menghapus? (y/n): ";
+        cin >> yakin;
+        if (yakin == 'y' || yakin == 'Y') {
+            for (
+                int i = indexKetemu;
+                i < jumlahKontak - 1;
+                i++
+            ) {
+                daftarKontak[i] = daftarKontak[i + 1];
+            }
 
-			if (action == 1) {
-				cout << "Nama Baru   : "; getline(cin >> ws, pointerEdit->nama);
-				cout << "No HP Baru  : "; cin >> pointerEdit->noTelepon;
-				cout << "Kategori Baru (1:Keluarga, 2:Saudara, 3:Teman, 4:Tetangga) : "; cin >> pointerEdit->kategori;
-				storeDatabase();
-				cout << "\n>> Data diperbarui!\n";
-			} else if (action == 2) {
-				for (int i = indexKetemu; i < jumlahKontak - 1; i++) {
-					daftarKontak[i] = daftarKontak[i+1];
-				}
-				jumlahKontak--;
-				storeDatabase();
-				cout << "\n>> Data dihapus!\n";
-			}
-		} else {
-			cout << "\n[ DATA TIDAK DITEMUKAN ]\n\n";
-			cout << "ulang (y/n) : " ; cin >> ulang;
-		}
-	} while (ulang == "y");
+            jumlahKontak--;
+
+            overwriteDatabase();
+
+            cout << ">> Data berhasil dihapus!\n";
+        }
+    }
 }
 
-void cinFail() { cin.clear();
-				 cin.ignore(1000, '\n');
-                 cout << "Input tidak valid. Silakan masukkan angka." << endl;
+// MENU STATISTIK
+void menuStatistik() {
+    system("cls");
+    cout << "\n=== STATISTIK ===\n";
+    cout << "Keluarga : "
+         << hitungPerKategori(0, 1)
+         << endl;
+
+    cout << "Saudara  : "
+         << hitungPerKategori(0, 2)
+         << endl;
+
+    cout << "Teman    : "
+         << hitungPerKategori(0, 3)
+         << endl;
+
+    cout << "Tetangga : "
+         << hitungPerKategori(0, 4)
+         << endl;
+
+    int f;
+    cout << "\nFilter kategori (1-4 / 0 tidak): ";
+    cin >> f;
+    if (f >= 1 && f <= 4) {
+        tampilkanTabel(f);
+    }
+    system("pause");
 }
 
+// MAIN
 int main() {
     loadDatabase();
     int pil;
     do {
         system("cls");
+
         cout << "\n====== MAIN MENU ======\n";
         cout << "1. Tambah Kontak\n";
-        cout << "2. Lihat Daftar Kontak\n";
-        cout << "3. Cari Kontak\n";
-        cout << "4. Statistik & Filter Kontak\n";
+        cout << "2. Lihat Kontak\n";
+        cout << "3. Cari/Edit/Hapus\n";
+        cout << "4. Statistik\n";
         cout << "5. Keluar\n";
-        cout << "Pilihan: "; cin >> pil;
-        if (cin.fail()) cinFail();
-		
+
+        cout << "Pilihan : ";
+
+        cin >> pil;
+
+        if (cin.fail()) {
+            cinFail();
+            continue;
+        }
+
         switch (pil) {
-            case 1:
-                system("cls");
-                tambahKontak();
-                system("pause");
-                break;
-            case 2:
-                system("cls");
-                int s;
-                do {
-					cout << "--- LIHAT DAFTAR KONTAK ---\n";
-					cout << "Urutkan Nama :\n1. ASC (A-Z)\n2. DESC (Z-A)\nPilih : "; cin >> s;
-				} while ( s != 1 && s != 2);
-					urutkanData(s == 1);
-					tampilkanTabel();
-                system("pause");
-                break;
-            case 3:
-                cariEditHapus();
-                system("pause");
-                break;
-            case 4:
-                system("cls");
-                cout << "\n--- STATISTIK KONTAK ---\n";
-                cout << "Keluarga : " << hitungPerKategori(0, 1) << endl;
-                cout << "Saudara  : " << hitungPerKategori(0, 2) << endl;
-                cout << "Teman    : " << hitungPerKategori(0, 3) << endl;
-                cout << "Tetangga : " << hitungPerKategori(0, 4) << endl;
-                
-                cout << "\nIngin tampilkan kategori tertentu?\n(1: Keluarga, 2: Saudara, 3: Teman, 4: Tetangga || 0 untuk tidak): ";
-                int f; cin >> f;
-                if (f > 0 and f < 5) tampilkanTabel(f);
-                system("pause");
-                break;
+        case 1:
+            system("cls");
+            tambahKontak();
+            system("pause");
+            break;
+        case 2: {
+            system("cls");
+            int s;
+            do {
+                cout << "1. ASC\n";
+                cout << "2. DESC\n";
+                cout << "Pilih : ";
+                cin >> s;
+            } while (s != 1 && s != 2);
+            quickSort(0,jumlahKontak - 1,s == 1);
+            tampilkanTabel();
+            system("pause");
+            break;
+        }
+        case 3:
+            system("cls");
+            cariEditHapus();
+            system("pause");
+            break;
+        case 4:
+            menuStatistik();
+            break;
+        case 5:
+            cout << "Program selesai.\n";
+            break;
+        default:
+            cout << "Menu tidak valid!\n";
         }
     } while (pil != 5);
-
-    cout << "\nProgram selesai. Sampai jumpa!\n";
     return 0;
-} 
+}
